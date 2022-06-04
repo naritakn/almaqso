4528a488503599f03c1f11023c4b3e1815e8aa25
Yuki Yoshimura <astro.yoshimura@gmail.com>
2022-06-04 15:47:16 +0900

    add check_and_run.py

---
+++ scripts/check_and_run.py
@@ -0,0 +1,23 @@
+import os
+import glob
+import numpy as np
+import sys
+import datetime
+
+args = sys.argv
+
+tarlist = glob.glob('*.tar')
+urllist = np.load(args[1])
+
+exec_tarlist = []
+
+for f in tarlist:
+    dd = urllist[urllist[:,0]=='https://almascience.eso.org/dataPortal/'+f][:,1].astype('int') - os.path.getsize(f)
+    if dd > 0:
+        print(f+' '+str(dd/1024/1024/1024))
+    else:
+        exec_tarlist.append(f)
+
+dt_now = datetime.datetime.now()
+ver = dt_now.isoformat().replace(':','-')
+np.save('exec_tarlist.v'+ver+'.npy',np.array(exec_tarlist))
